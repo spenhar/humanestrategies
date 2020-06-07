e90f00fcb214d798b3b67abc9cf05cab2110cac4
Taylor W Brown <taylorwhittenbrown@gmail.com>
2020-06-07 19:14:02 -0400

    Update strategies.js

--- strategies.js
+++ strategies.js
@@ -1,6 +1,17 @@
 var strategy = [
-            "Who is your representative?",  
-            "Can you protest without a sign?"
+            "Who is your mayor?",  
+            "Can you protest without a sign?",
+            "What else could you do other than call the police? Be prepared.",
+            "Read about microaggressions.",
+            "You have a white audience that the Black community may not have. Start a conversation about race.",
+            "Write 10 sentences about what 'whiteness' means to you.",
+            "'I dont see color' ingores our privilege and erases the reality that our experiences differ greatly.",
+            "What proportion of Black Americans are incarcerated? What proportion of White Americans?",
+            "How long did slavery last in America?",
+            "When did African Americans gain the right to vote?",
+            "What is 'gerrymandering'.",
+            "How much does your state spend on the police? How much does it spend on education?",
+            "Read the history of the NAACP: https://www.history.com/topics/civil-rights-movement/naacp"            
             ];
 
 function choose(){
