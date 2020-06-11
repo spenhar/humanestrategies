78365b1423d7a3d8b39d5ae59ce535ac322917db
Taylor W Brown <taylorwhittenbrown@gmail.com>
2020-06-11 19:56:52 -0400

    Update strategies.js

--- strategies.js
+++ strategies.js
@@ -11,7 +11,30 @@ var strategies = [
             "When did African Americans gain the right to vote?",
             "What is 'gerrymandering'.",
             "How much does your state spend on the police? How much does it spend on education?",
-            "Read the history of the NAACP: https://www.history.com/topics/civil-rights-movement/naacp"            
+            "Read the history of the NAACP: https://www.history.com/topics/civil-rights-movement/naacp",
+            "Do you see yourself as 'one of the good ones'? Make an argument against that.",
+            "There is a history of white women's racism within feminism. Read about it.",
+            "What is your role in racist systems?",
+            "In what ways is the medical system in America racist?",
+            "What are you defending right now?",
+            "In what way is 'normal' to you racist?",
+            "Why do we allow destructive rioting after sporting events?",
+            "Have you gotten more concerned about property damage than lives lost?",
+            "If a police officer does harm, what processes in place for reprecussions?",
+            "What is the historical relationship between white supremacy and the police?",
+            "Why do or don't you post about what you believe on social media?",
+            "What does the phrase 'white silence is white violence' mean to you?",
+            "As a child, were you ever the only one who looked like you at a party? As an adult?",
+            "Have you ever been singled out by a teacher because of your race?",
+            "When you turn on the TV, do you mostly see people who look like you? Why?",
+            "Per capita, how many people died in police custody last year in the US? In Germany? In the UK?",
+            "Per capita, how many people were arrested last year in the United States? In Australia? In Sweden?",
+            "Per capita, how many people are in prison or jail in the United States? How does this compare to other countries?",
+            "What was the Tulsa Race Massacre?",
+            "What proportion of the United States is white?",
+            "What is your favorite book by a Black author? Your second?",
+            "What is your favorite movie with a Black protagonist?",
+            "How large is the income gap between Blacks and whites in America? And the wealth gap?"
             ];
 
 function choose(){
