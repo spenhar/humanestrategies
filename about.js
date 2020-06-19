e7d9e0bef458159d5a661b1183162b0325fbcdca
Spencer Harrison <spencerjharrison@gmail.com>
2020-06-19 13:43:03 -0400

    added about prompt fixed author

---
+++ about.js
@@ -0,0 +1,14 @@
+(function() {
+  document.addEventListener('click', function(event) {
+    if (event.target.tagName !== 'A') {
+      window.location.reload();
+    }
+  });
+
+  document.querySelector('.about-toggle').addEventListener('click', function(event) {
+    event.preventDefault();
+    var about = document.querySelector('#about');
+    this.innerHTML = about.classList.contains('visible') ? '?' : '&times;';
+    about.classList.toggle('visible');
+  });
+}());
